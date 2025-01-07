97471bce4c709723e17b69e573f62b2326c13037
appdude117 <browne.liam@outlook.com>
2025-01-07 22:45:52 +1100

    slow beginning

---
+++ js.js
@@ -0,0 +1,10 @@
+const bigcont = document.querySelector("#bigcont");
+const gridsq = document.createElement("gridsq");
+
+gridsq.setAttribute("id", "gridsq");
+gridsq.style.backgroundColor = "black";
+bigcont.appendChild(gridsq)
+gridsq.setAttribute("style", " background: black; display: flex ; height: 100px; width: 100px ; opacity: 100%;");
+gridsq.style.backgroundColor = "black";
+
+
