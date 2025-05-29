4c7bae58db960c294adfa23e692cda89f7d9217e
appdude117 <browne.liam@outlook.com>
2025-05-29 20:41:59 +1000

    edit got grid working

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 const bigcont = document.querySelector("#bigcont");
-const gridsq = document.createElement("gridsq");
-
-gridsq.setAttribute("id", "gridsq");
-gridsq.style.backgroundColor = "black";
-bigcont.appendChild(gridsq)
-gridsq.setAttribute("style", " background: black; display: flex ; height: 100px; width: 100px ; opacity: 100%;");
-gridsq.style.backgroundColor = "black";
 
 
+for (let i = 0; i < 256; i++) {
+    var gridsq = document.createElement('gridsq');
+    gridsq.setAttribute("style", "height: 5.75%; display: flex ; opacity: 100%; flex: 1 0 5.75%; border: 0.5%; border-style: solid ;");
+    gridsq.style.backgroundColor = "grey";
+    gridsq.style.borderColor = 'blue';
+    bigcont.appendChild(gridsq);
+}
