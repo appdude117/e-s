1ab1055a8bfd628a9d13aedde2a4359a94a44581
appdude117 <browne.liam@outlook.com>
2025-06-03 19:18:49 +1000

    yoooo it sizes

--- js.js
+++ js.js
@@ -3,8 +3,7 @@ const bigcont = document.querySelector("#bigcont");
 
 for (let i = 0; i < 256; i++) {
     var gridsq = document.createElement('gridsq');
-    gridsq.setAttribute("style", "height: 5.75%; display: flex ; opacity: 100%; flex: 1 0 5.75%; border: 0.5%; border-style: solid ;");
-    gridsq.style.backgroundColor = "grey";
-    gridsq.style.borderColor = 'blue';
+    gridsq.setAttribute("style", "height: 5.75%; display: flex ; opacity: 100%; flex: 1 0 5.75%; gap: 0.5%; row-gap: 0.5% ; width 5.75%");
+    gridsq.style.backgroundColor = "black";
     bigcont.appendChild(gridsq);
 }
